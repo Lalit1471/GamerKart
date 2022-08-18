9d70a17de175bb6a44072b6a6f575d93aab8de5e
iamlalit <alalitkumar02@gmail.com>
2022-08-18 19:15:12 +0530

    Routed LoginPage to Homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_app/pages/loginpage.dart';
 import 'package:flutter/material.dart';
 import 'pages/homepage.dart';
+import 'utils/routes.dart';
 
 void main()
 {
@@ -14,13 +15,14 @@ class MyApp extends StatelessWidget
   Widget build(BuildContext context)
   {
     return MaterialApp(
+      debugShowCheckedModeBanner: true,
       themeMode: ThemeMode.light,
       darkTheme: ThemeData(brightness: Brightness.dark),
       // initialRoute: "/home",
       routes: {
         "/" : (context)=>const LoginPage(),
-        "/home" : (context)=>const Homepage(),
-        "/login" : (context)=>const LoginPage(),
+        MyRoutes.homeRoute : (context)=>const Homepage(),
+        MyRoutes.loginRoute : (context)=>const LoginPage(),
       },
     );
   }
